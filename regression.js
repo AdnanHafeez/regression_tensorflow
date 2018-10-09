f796a8c03da972d08de337d600916156ea834a67
AdnanHafeez <adnanhr81@gmail.com>
2018-10-10 10:11:41 +1100

    Added some code for training

--- regression.js
+++ regression.js
@@ -1,1 +1,24 @@
 //Tensorflow model for regression fitting
+import * as tf from '@tensorflow/tfjs';
+
+const a = tf.variable(tf.scalar(Math.random()));
+const b = tf.variable(tf.scalar(Math.random()));
+const c = tf.variable(tf.scalar(Math.random()));
+const d = tf.variable(tf.scalar(Math.random()));
+
+
+function predict(x) {
+  return tf.tidy(() => {
+    return a.mul(x.pow(tf.scalar(3)))
+      .add(b.mul(x.square()))
+      .add(c.mul(x))
+      .add(d);
+  });
+}
+
+console.log(predict(1));
+
+function loss(predicitons,labels) {
+  const meanSquaredError = predictions.sub(label).square().mean();
+  return meanSquaredError;
+}
